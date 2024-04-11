048bfd6675aad5ed7711724fae384b4626b240b8
Joseph <josephshonko99@gmail.com>
2024-04-11 14:40:55 +0300

    commiting phase 1 project

--- mains.js
+++ mains.js
@@ -1,15 +1,17 @@
 document.getElementById('appointmentForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the default form submission
-    
-    // You can handle the form submission here, for example, sending the form data to a server using AJAX
-    // For demonstration purposes, let's just log the form data to the console
+    event.preventDefault(); 
+
     const formData = new FormData(event.target);
     const formObject = {};
     formData.forEach((value, key) => {
       formObject[key] = value;
     });
+
     console.log(formObject);
-    
-    // After handling the form submission, you can perform any additional actions, such as showing a confirmation message
-    alert('Appointment submitted successfully!');
-  });
+
+    if (statemen => true) {
+        alert('Thank you for choosing Enashipae nails bar, your application was successfully submitted!');
+    } else {
+        alert('There was an error submitting your application. Please try again later.');
+    }
+});
