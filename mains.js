7f8eb30b4b8146711eea7229f739201e00f7c86e
Joseph <josephshonko99@gmail.com>
2024-04-12 12:30:41 +0300

    commiting phase 1 project

--- mains.js
+++ mains.js
@@ -1,3 +1,4 @@
+
 document.getElementById('appointmentForm').addEventListener('submit', function(event) {
     event.preventDefault(); 
 
@@ -8,10 +9,18 @@ document.getElementById('appointmentForm').addEventListener('submit', function(e
     });
 
     console.log(formObject);
+    // I used the "IF ELSE statement" in the submiting form
 
-    if (statemen => true) {
+    if (true) { 
         alert('Thank you for choosing Enashipae nails bar, your application was successfully submitted!');
     } else {
         alert('There was an error submitting your application. Please try again later.');
     }
 });
+
+
+
+// uding the IIFE functin  a comment for welcoming the customer
+const greeting = function() {
+    console.log('Hello customer, how may I help you??');
+}();
